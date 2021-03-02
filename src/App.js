c443704657d03d2f3d16bbc565dd56a44944bd79
rvasquez <rvasquez1@gmail.com>
2021-03-01 23:05:49 -0500

    changing contact and portfolio page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function About() {
+  return (
+    <div>
+      <Hero backgroundImage="./img/scuba.png">
+        <h1>Rafael Vasquez</h1>
+        <h2>Full Stack Developer</h2>
+      </Hero>
+      <Container style={{ marginTop: 10 }}>
+        <Row>
+          <Col size="md-12">
+          <article className="container-fluid p-5">
+    <div className="container last">
+      
+      <div className="d-flex p-3 flex-column">
+        
+        <Col size="md-12" className="col-xs-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 ml-auto mr-auto bg-light border-bottom rounded-top">
+          <h1 class="display-4"><u>About Me</u></h1>
+        </Col>
+        
+        <Col size="md-12"
+          className="col-xs-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 text-justify ml-auto mr-auto bg-light rounded-bottom">
+          <p>I was born in 1983 in Puerto Plata, Dominican Republic. I currently live in Orlando, Fl. Father of two
+            beautiful daughters and have my own online company for selling used refrigerator parts on eBay.
+          </p>
+          
+          <p class="mt-3">
+            I graduated in college of BEng. Computing and System Engineering in 2005 in the PUCMM, Sto Dgo, DR. After i
+            went to Buenos Aires, Argentina and made a MBA that i graduated in 2008. After that i went to Barcelona,
+            Spain and did a Master in Marketing. I've been working in different fields in my live from server, bartender
+            to Manager of Projects in Engineering.
+          </p>
+          <Row size="md-12">
+            <Col size="md-6">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item list-group-item-info font-weight-bold">Hobbies:</li>
+                <li className="list-group-item list-group-item-ligh">Scubadiving</li>
+                <li className="list-group-item list-group-item-ligh">Reading Chinese Novels</li>
+                <li className="list-group-item list-group-item-ligh">Gaming</li>
+                <li className="list-group-item list-group-item-ligh">Outdoor Activities</li>
+              </ul>
+            </Col>
+            <Col size="md-6">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item list-group-item-info font-weight-bold">Interest:</li>
+                <li className="list-group-item list-group-item-ligh">Javascript</li>
+                <li className="list-group-item list-group-item-ligh">Sports</li>
+                <li className="list-group-item list-group-item-ligh">Photography</li>
+                <li className="list-group-item list-group-item-ligh">Travel</li>
+              </ul>
+            </Col>
+          </Row>
+        </Col>
+      </div>
+    </div>
+  </article>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default About;
